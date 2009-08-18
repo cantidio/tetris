d207c4a4ad0505d373b45852e07255babd6208e9
Cantidio Fontes <aniquilatorbloody@gmail.com>
2009-08-18 00:23:47 +0000

    - Teste

--- Tetris/build/Debug/GNU-Linux-x86/_ext/home/cantidio/Development/tetris/src/brick.o.d
+++
@@ -1,9 +0,0 @@
- \
- build/Debug/GNU-Linux-x86/_ext/home/cantidio/Development/tetris/src/brick.o:  \
- /home/cantidio/Development/tetris/src/brick.cpp \
-  /home/cantidio/Development/tetris/src/../include/brick.hpp \
-  /home/cantidio/Development/gorgon++/src/geometry/include/gorgon_point.hpp
-
-/home/cantidio/Development/tetris/src/../include/brick.hpp:
-
-/home/cantidio/Development/gorgon++/src/geometry/include/gorgon_point.hpp:
